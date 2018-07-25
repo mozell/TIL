6595643dea54d5f19a6876ee939915e48a299465
mozell <mozellatto@gmail.com>
2018-07-25 16:40:00 +0900

    q2606 fail

--- onlineAlgo/Baekjoon/src/Q2606.java
+++ onlineAlgo/Baekjoon/src/Q2606.java
@@ -1,19 +1,60 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Q2606 {
+public class Q2606 {	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int com = sc.nextInt();
 		int edge = sc.nextInt();
-		int[][] network = new int[com][];
 		
-		for(int i = 0; i < network.length; i++) {
-			network[i][0] = i+1;
+		ArrayList<Integer> network[] = new ArrayList[edge];
+		for(int i=0; i<edge; i++) {
+			network[i] = new ArrayList<Integer>(); 
 		}
+
 		
-		for(int i = 0; i < edge; i++) {
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		network[0].add(n);
+		network[0].add(m);
+		
+		for(int i = 1; i < edge; i++) {
+			n = sc.nextInt();
+			m = sc.nextInt();
 			
+			for(int j = 0; j < edge; j++) {
+				if(network[j].contains(n)) {
+					if(!network[j].contains(m))
+						network[j].add(m);
+					break;
+				}
+				else if(network[j].contains(m)) {
+					if(!network[j].contains(n))
+						network[j].add(n);
+					break;
+				}
+				//처음들어왓넹? 그러면 빈 네트워크에다가  채우자
+				else {
+					for(int k = 0; k < edge; k++) {
+						if(network[k].isEmpty()) {
+							network[k].add(n);
+							network[k].add(m);
+							break;
+						}
+					}
+				}
+			}
 		}
+		
+		for(int i = 0; i<edge; i++) {
+			if(network[i].contains(1)) {
+				System.out.println(network[i].size()-1);
+			}
+			else
+				System.out.println("0");
+		}
+		sc.close();
+		
 	}
 }
+
