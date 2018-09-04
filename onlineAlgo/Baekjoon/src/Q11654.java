ac96c3cafe5619cf1bfac1c0aab70b756643ad5b
mozell <mozellatto@gmail.com>
2018-09-04 22:23:38 +0900

    q11654

---
+++ onlineAlgo/Baekjoon/src/Q11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Q11654{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println(Integer.valueOf(c));
+        sc.close();
+    }
+}
